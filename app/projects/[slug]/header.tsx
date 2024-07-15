6bffa5e564197bde4e74000e35fbeb476646f44b
Alex Franko <100057117+sanfranko@users.noreply.github.com>
2024-07-15 13:50:25 +0600

    Update header.tsx

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -3,6 +3,8 @@ import { ArrowLeft, Eye, Github, Instagram } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
+export const maxDuration = 300;
+
 type Props = {
 	project: {
 		url?: string;
